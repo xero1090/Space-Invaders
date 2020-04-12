a6c6b51eb4c84ae5face5e18a09c325bfe94cc51
PeanutSpread <LukeVanRooyen@hotmail.com>
2020-04-12 18:36:47 -0400

    Additions: - Added a struct for location on an xy plane - Added a base abstract class for all characters CharInstance.
    Changes:
    - Removed the PlayerTurret class for now, will be reimplemented later
    - Player folder renamed to Characters

---
+++ SpaceInvaders/Characters/CharInstance.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Windows.UI.Xaml.Media.Imaging;
+
+public struct location
+{
+	private int _xLocation;
+	private int _yLocation;
+
+	public int X
+	{ get { return _xLocation; } set { _xLocation = value; } }
+	public int Y
+	{ get { return _yLocation; } set { _yLocation = value; } }
+
+	public location(int xLoc, int yLoc)
+    {
+		_xLocation = xLoc;
+		_yLocation = yLoc;
+    }
+}
+
+public abstract class CharInstance
+{
+
+	protected location _location;
+	protected bool _isAlive;
+	protected BitmapImage _sprite;
+	
+	public location Location
+	{ get { return _location; } }
+
+	public bool IsAlive
+	{ get { return _isAlive; } set { _isAlive = value; } }
+
+	public BitmapImage Sprite
+	{ get { return _sprite; } set { _sprite = value; } }
+
+	public CharInstance(int xStart, int yStart)
+	{
+		_location = new location(xStart, yStart);
+		_isAlive = true;
+	}
+
+	public bool IsHit()
+    {
+		//TODO: figure this crud out
+    }
+
+	public void Move(int xMod, int yMod)
+    {
+		_location.X += xMod;
+		_location.Y += yMod;
+    }
+
+	public void MoveTo(int xSet, int ySet)
+    {
+		_location.X = xSet;
+		_location.Y = ySet;
+	}
+
+	public virtual void OnDestruction()
+    {
+		//TODO: Specific crap when you die
+    }
+}
