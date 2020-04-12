34ffc03f6cc0f1503f46ad9be29a3127aed67c6e
PeanutSpread <LukeVanRooyen@hotmail.com>
2020-04-12 19:05:51 -0400

    IMPORTANT Moved the characters folder to the actual solution

--- SpaceInvaders/Characters/CharInstance.cs
+++ SpaceInvaders/Characters/CharInstance.cs
@@ -1,64 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.UI.Xaml.Media.Imaging;
 
-public struct location
+
+namespace SpaceInvaders
 {
-	private int _xLocation;
-	private int _yLocation;
-
-	public int X
-	{ get { return _xLocation; } set { _xLocation = value; } }
-	public int Y
-	{ get { return _yLocation; } set { _yLocation = value; } }
-
-	public location(int xLoc, int yLoc)
-    {
-		_xLocation = xLoc;
-		_yLocation = yLoc;
-    }
-}
-
-public abstract class CharInstance
-{
-
-	protected location _location;
-	protected bool _isAlive;
-	protected BitmapImage _sprite;
-	
-	public location Location
-	{ get { return _location; } }
-
-	public bool IsAlive
-	{ get { return _isAlive; } set { _isAlive = value; } }
-
-	public BitmapImage Sprite
-	{ get { return _sprite; } set { _sprite = value; } }
-
-	public CharInstance(int xStart, int yStart)
+	public struct location
 	{
-		_location = new location(xStart, yStart);
-		_isAlive = true;
+		private int _xLocation;
+		private int _yLocation;
+
+		public int X
+		{ get { return _xLocation; } set { _xLocation = value; } }
+		public int Y
+		{ get { return _yLocation; } set { _yLocation = value; } }
+
+		public location(int xLoc, int yLoc)
+		{
+			_xLocation = xLoc;
+			_yLocation = yLoc;
+		}
 	}
 
-	public bool IsHit()
-    {
-		//TODO: figure this crud out
-    }
+	public abstract class CharInstance
+	{
 
-	public void Move(int xMod, int yMod)
-    {
-		_location.X += xMod;
-		_location.Y += yMod;
-    }
+		protected location _location;
+		protected bool _isAlive;
+		protected BitmapImage _sprite;
 
-	public void MoveTo(int xSet, int ySet)
-    {
-		_location.X = xSet;
-		_location.Y = ySet;
+		public location Location
+		{ get { return _location; } }
+
+		public bool IsAlive
+		{ get { return _isAlive; } set { _isAlive = value; } }
+
+		public BitmapImage Sprite
+		{ get { return _sprite; } set { _sprite = value; } }
+
+		public CharInstance(int xStart, int yStart)
+		{
+			_location = new location(xStart, yStart);
+			_isAlive = true;
+		}
+
+		public bool IsHit()
+		{
+			//TODO: figure this crud out
+		}
+
+		public void Move(int xMod, int yMod)
+		{
+			_location.X += xMod;
+			_location.Y += yMod;
+		}
+
+		public void MoveTo(int xSet, int ySet)
+		{
+			_location.X = xSet;
+			_location.Y = ySet;
+		}
+
+		public virtual void OnDestruction()
+		{
+			//TODO: Specific crap when you die
+		}
 	}
-
-	public virtual void OnDestruction()
-    {
-		//TODO: Specific crap when you die
-    }
 }
---
+++ SpaceInvaders/Characters/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+	public class Enemy : CharInstance
+	{
+		public Enemy(int xStart, int yStart, BitmapImage enemySprite) : base(xStart, yStart)
+		{
+
+			base._sprite = enemySprite;
+
+		}
+	}
+}
