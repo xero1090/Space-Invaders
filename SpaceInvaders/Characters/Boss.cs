aba3b774a48b023b6b6627394b2b5d09efde6762
Kevin Tran <tran94@sheridancollege.ca>
2020-04-21 17:15:25 -0400

    Added boss field variables, new Boss fight Page, added boss sprite.

--- SpaceInvaders/Characters/Boss.cs
+++ SpaceInvaders/Characters/Boss.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Shapes;
 
@@ -10,9 +11,27 @@ namespace SpaceInvaders.Characters
 {
     class Boss : CharInstance
     {
+        private byte health = 20;
+        private byte _hp;
+        private Brush _spriteShoot;
+
+        public byte hitPoints
+        { get { return _hp; } set { _hp = value; } }
         public Boss(double xStart, double yStart, BitmapImage bossSprite, Rectangle obj) : base(xStart, yStart)
         {
+            _hp = health;
             base._sprite = bossSprite;
+        }
+        public void ShootMissile()
+        {
+            _obj.Fill = _spriteShoot;
+        }
+        public void OnHit()
+        {
+            --_hp;
+            //TODO: when you die
+
+
         }
     }
 }
